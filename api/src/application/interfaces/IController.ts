50d3d6f114254bcc1578d8954257d52be2d552f6
Matheus Lopes <matheus.lopesmdev@gmail.com>
2025-03-16 10:56:26 -0300

    creating controller layers

---
+++ api/src/application/interfaces/IController.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export interface IRequest {
+    body: Record<string, any>;
+}
+
+export interface IResponse {
+    statusCode: number;
+    body: Record<string, any> | null;
+}
+
+export interface IController {
+    handle(request: IRequest): Promise<IResponse>;
+}
--- api/src/application/useCases/SignUpUseCase.ts
+++ api/src/application/useCases/SignUpUseCase.ts
@@ -11,6 +11,8 @@ interface IInput {
 type IOutput = void;
 
 export class SignUpUseCase {
+    constructor(private readonly salt: number) { }
+
     async execute({ email, name, password }: IInput): Promise<IOutput> {
         const accountAlreadyExists = await prismaClient.account.findUnique({
             where: { email },
@@ -20,7 +22,7 @@ export class SignUpUseCase {
             throw new AccountAlreadyExists();
         }
 
-        const hashedPassword = await hash(password, 10);
+        const hashedPassword = await hash(password, this.salt);
 
         await prismaClient.account.create({
             data: {
