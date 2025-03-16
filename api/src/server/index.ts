41c2398aa9b547cbb9b9535e91ab2c5d4401a5e6
Matheus Lopes <matheus.lopesmdev@gmail.com>
2025-03-16 14:58:58 -0300

    creating routeAdapter

--- api/src/server/index.ts
+++ api/src/server/index.ts
@@ -1,34 +1,13 @@
 import express from 'express';
-import { SignUpController } from '../application/controllers/SignUpController';
-import { SignInController } from '../application/controllers/SignInController';
-import { SignUpUseCase } from '../application/useCases/SignUpUseCase';
-import { SignInUseCase } from '../application/useCases/SignInUseCase';
+import { makeSignUpController } from "../factories/makeSignUpController"
+import { makeSignInController } from "../factories/makeSignInController"
+import { routeAdapter } from './adapters/routeAdapter';
 
 const app = express();
 app.use(express.json());
 
-app.use('/sign-up', async (request, response) => {
-    const SALT = 10;
-    const signUpUseCase = new SignUpUseCase(SALT);
-    const signUpController = new SignUpController(signUpUseCase);
-
-    const { statusCode, body } = await signUpController.handle({
-        body: request.body,
-    });
-
-    response.status(statusCode).json(body);
-});
-
-app.use('/sign-in', async (request, response) => {
-    const signInUseCase = new SignInUseCase();
-    const signInController = new SignInController(signInUseCase);
-
-    const { body, statusCode } = await signInController.handle({
-        body: request.body,
-    })
-
-    response.status(statusCode).json(body);
-});
+app.post('/sign-up', routeAdapter(makeSignUpController()));
+app.post('/sign-in', routeAdapter(makeSignInController()));
 
 app.listen(3001, () => {
     console.log('Server started!')
